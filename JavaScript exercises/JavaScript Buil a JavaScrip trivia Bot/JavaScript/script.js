86473abb03a95d3dd8199b153e4d39b5c0a92559
algie06 <algie061993@gmail.com>
2025-07-14 15:08:21 +0800

    trivia bot

---
+++ JavaScript exercises/JavaScript Buil a JavaScrip trivia Bot/JavaScript/script.js
@@ -0,0 +1,28 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "AddBot";
+let botLocation = "CSGo";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + "is a computer programming language.";
+console.log(codingFact);
+
+codingFact =
+  favoriteLanguage +
+  " is a high-level, often just-in-time compiled language that is dynamically typed and conforms to the ECMAScript standard.";
+console.log(codingFact);
+
+codingFact =
+  favoriteLanguage +
+  " It is primarily known as the scripting language for web pages, enabling interactive and dynamic content, but also has applications in server-side development (Node.js) and other areas. ";
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    "."
+);
