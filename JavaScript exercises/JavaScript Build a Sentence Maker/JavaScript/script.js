ef32b8fc261a6403d88d931769427a6728d278fb
algie06 <algie061993@gmail.com>
2025-07-14 16:06:01 +0800

    Sentence Maker

---
+++ JavaScript exercises/JavaScript Build a Sentence Maker/JavaScript/script.js
@@ -0,0 +1,51 @@
+let adjective = "happy";
+let noun = "dragon";
+let verb = "angry";
+let place = "cave";
+let adjective2 = "flaming";
+let noun2 = "pizza";
+
+let firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "sad";
+noun = "tiger";
+verb = "sleepy";
+place = "forest";
+adjective2 = "sparkly";
+noun2 = "ice cream";
+
+let secondStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("Second story: " + secondStory);
