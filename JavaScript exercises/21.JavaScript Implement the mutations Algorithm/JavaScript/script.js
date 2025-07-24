0948a2fb248971c5a996fd9128c6c22f7740115d
algie06 <algie061993@gmail.com>
2025-07-24 16:48:17 +0800

    mutation usong for loop

---
+++ JavaScript exercises/21.JavaScript Implement the mutations Algorithm/JavaScript/script.js
@@ -0,0 +1,11 @@
+const mutation = (arr) => {
+  const firstStr = arr[0].toLowerCase();
+  const secondStr = arr[1].toLowerCase();
+  for (let i = 0; i < secondStr.length; i++) {
+    if (firstStr.indexOf(secondStr[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+};
+console.log(mutation(["hello", "Hello"]));
