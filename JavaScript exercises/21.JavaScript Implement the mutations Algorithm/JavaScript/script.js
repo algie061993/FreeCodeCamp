6b6078644a01d989332cb1dec60cb6870858d7d4
algie06 <algie061993@gmail.com>
2025-07-24 16:55:45 +0800

    mutation usong for..of loop

--- JavaScript exercises/21.JavaScript Implement the mutations Algorithm/JavaScript/script.js
+++ JavaScript exercises/21.JavaScript Implement the mutations Algorithm/JavaScript/script.js
@@ -1,11 +1,22 @@
 const mutation = (arr) => {
   const firstStr = arr[0].toLowerCase();
   const secondStr = arr[1].toLowerCase();
-  for (let i = 0; i < secondStr.length; i++) {
-    if (firstStr.indexOf(secondStr[i]) === -1) {
+  for (const char of secondStr) {
+    if (firstStr.indexOf(char) === -1) {
       return false;
     }
   }
   return true;
 };
-console.log(mutation(["hello", "Hello"]));
+
+const arr1 = ["hello", "Hello"];
+const result = mutation(arr1);
+console.log(`The result of the mutation of the ${arr1} is ${result}.`);
+
+const arr2 = ["hello", "hey"];
+const result2 = mutation(arr2);
+console.log(`The result of the mutation of the ${arr2} is ${result2}.`);
+
+const arr3 = ["Alien", "line"];
+const result3 = mutation(arr3);
+console.log(`The result of the mutation of the ${arr3} is ${result3}.`);
