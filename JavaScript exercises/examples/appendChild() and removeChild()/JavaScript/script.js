69438f77f82de61bee5a707f0936db89f08d8c36
algie061993 <algie061993@gmail.com>
2025-08-18 13:26:24 +0800

    addpendChild() and removeChild()

--- JavaScript exercises/examples/appendChild() and removeChild()/JavaScript/script.js
+++ JavaScript exercises/examples/appendChild() and removeChild()/JavaScript/script.js
@@ -2,8 +2,11 @@ const addButton = document.getElementById("addButton");
 const removeButton = document.getElementById("removeButton");
 const itemList = document.getElementById("itemList");
 const message = document.getElementById("message");
+//added by appendChild()  function
+// This function adds a new item to the list
+// and updates the message accordingly.
 
-addButton.addEventListener("click", function () {
+function addItem() {
   let addedItemLimit = 5;
   if (itemList.children.length >= addedItemLimit) {
     message.textContent = "Cannot add more than " + addedItemLimit + " items!";
@@ -14,20 +17,23 @@ addButton.addEventListener("click", function () {
     "Added by appendChild() - Item " + (itemList.children.length + 1);
   itemList.appendChild(newItem);
   message.textContent = `Added item: ${newItem.textContent}. You can remove items now!`;
-});
+}
 
-removeButton.addEventListener("click", function () {
+addButton.addEventListener("click", addItem);
+
+//added by removeChild()  function
+// This function removes the last item from the list
+// and updates the message accordingly.
+function removeLastItem() {
   const lastItem = itemList.lastElementChild;
-  if (lastItem) {
-    if (itemList.children.length === 2) {
-      message.textContent = `You can remove further items!`;
-    } else {
-      itemList.removeChild(lastItem);
-      message.textContent = `Removed the last item by removeChild(): ${
-        lastItem.textContent.split(" - ")[1]
-      }.`;
-    }
+
+  if (itemList.children.length === 2) {
+    message.textContent = `You can remove further items! Last item: ${itemList.children[1].textContent}.`;
   } else {
-    message.textContent = "No items to remove!";
+    itemList.removeChild(lastItem);
+    message.textContent = `Removed the last item by removeChild(): ${
+      lastItem.textContent.split(" - ")[1]
+    }.`;
   }
-});
+}
+removeButton.addEventListener("click", removeLastItem);
